fd35e37e19e75e0733e0d9a21de2ae30774414a3
vitor costa <vitormoreira128@gmail.com>
2021-10-09 09:51:51 -0300

    inicio de projeto

---
+++ aula1/js/script.js
@@ -0,0 +1,17 @@
+
+/* document.getElementsByTagName("h1")[0].innerHTML="mudar texto";
+
+var idade =10;
+let email ="joao@terra.com.br";
+if (10==10){
+    var nome ="pedro";
+let sobrenome ="oliveira";
+}
+
+console.log(nome);
+console.log(sobrenome);*/
+// document.getElementById("saida").innerHTML= "Menssagem";
+// document.getElementById("saida").style.background="yellow";
+// document.getElementById("saida").style.color="red";
+var tag = document.getElementById ("saida");
+tag.style
